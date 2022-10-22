0e078d9f984a110979b2e9991104fcb542ddaab6
itsnotmikowhy <111461945+itsnotmikowhy@users.noreply.github.com>
2022-10-22 18:02:40 +0200

    now is complete

---
+++ number_guessing_game.py
@@ -0,0 +1,8 @@
+# gra polegająca na zgadnięciu jaki numer komputer wylosował
+
+# zacznij jakim numberem chce user mieć maks range w grze
+# zabezpiecz odpowiedź od stringow(isdigit) i liczb ponizej zera. Jezeli sie nie zgadza to quit
+# niech komputer wybierze randomowa liczbe za pomoca randomint(0, wybor usera)
+# uyj while true by user zgadywal numer jaki komputer wylosowal
+# podlicz ile razy ktos probowal razy zgadanac number
+# mozna ulepszyc gre za pomocą dodania ifelse który wskaze czy nie przeskoczylismy numeru
