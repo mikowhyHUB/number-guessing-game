c47c5221ce9da3249af34900bf7d4d6b1d89cc82
itsnotmikowhy <111461945+itsnotmikowhy@users.noreply.github.com>
2022-10-23 15:52:45 +0200

    added first futures. Not working as suppose to.

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -1,8 +1,24 @@
-# gra polegająca na zgadnięciu jaki numer komputer wylosował
+
+
+from curses.ascii import isdigit
+from random import randint
 
 # zacznij jakim numberem chce user mieć maks range w grze
-# zabezpiecz odpowiedź od stringow(isdigit) i liczb ponizej zera. Jezeli sie nie zgadza to quit
+maks_numb = int(input('What number would like to pick to be maks number: '))
+
 # niech komputer wybierze randomowa liczbe za pomoca randomint(0, wybor usera)
+random_numb = randint(1, maks_numb)
+# zabezpiecz odpowiedź od stringow(isdigit) i liczb ponizej zera. Jezeli sie nie zgadza to quit
+guess = int(input('Pick a number: '))
+while True:
+    if guess == random_numb:
+        print('Congratz, you got it!')
+        quit()
+    elif guess < 1:
+        print('only numbers above 1')
+        print('wrong number')
+
+
 # uyj while true by user zgadywal numer jaki komputer wylosowal
 # podlicz ile razy ktos probowal razy zgadanac number
 # mozna ulepszyc gre za pomocą dodania ifelse który wskaze czy nie przeskoczylismy numeru
